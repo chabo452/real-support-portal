2ebb0b5dc372f0c663b72595796f6b588e2f2479
Arc-lead <info@arc-lead.co.jp>
2025-06-17 14:25:40 +0900

    fix: index.html and build output

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/real-support-portal/',
   plugins: [react()],
-  publicDir: 'public',
   build: {
-    outDir: 'docs'  // GitHub Pages 用
+    outDir: 'docs'
   }
 })
