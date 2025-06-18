692b210182601ac3586367e43d698be05eab8c89
Arc-lead <info@arc-lead.co.jp>
2025-06-18 16:51:02 +0900

    VSCode上の状態で完全復元

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/real-support-portal/', // GitHub Pages のパスプレフィックス
-  plugins: [react()],
+  base: '/real-support-portal/',
   build: {
-    outDir: 'docs', // GitHub Pages用にdocsに出力
-    rollupOptions: {
-      input: './index.html', // メインエントリーポイント
-      onwarn(warning, warn) {
-        if (warning.message.includes('css-syntax-error')) return
-        warn(warning)
-      },
-    },
-    emptyOutDir: false // これを追加することで upload.html や jobData.json を消さない
+    outDir: 'dist/docs'
   },
+  plugins: [react()]
 })
