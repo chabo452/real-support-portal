ad7fc31051fafe0a1b41a37b936ffc126543aed9
Arc-lead <info@arc-lead.co.jp>
2025-06-18 15:32:18 +0900

    Deploy to gh-pages

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/real-support-portal/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist/docs',
+    emptyOutDir: true
+  }
+})
