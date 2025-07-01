fc98ab2ca1c6b33c5a4c1f0c9ee93f64bbfa0dc8
noguchi <noguchi@n-g-office.com>
2025-07-01 22:32:09 +0900

    求人CSV一覧・タグ分類ページ 更新

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS JobPostings (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    CompanyName TEXT,
+    JobTitle TEXT,
+    JobDescription TEXT,
+    DangerLevel INTEGER,
+    PostedDate TEXT,
+    ExpireDate TEXT
+);
